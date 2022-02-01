07bcebcbd22b28a54510c186ce2624d001af4d8f
Adrian Reyes <adrian_reyes1@yahoo.com>
2022-01-31 18:34:24 -0800

    show page is operational again

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -6,29 +6,38 @@ function show (data) {
     return (
         <Default>
           <main>
-        <h1>
-            {data.place.name}
-        </h1>
-        <img src={data.place.pic} />
+            <div className="row">
+              <div className="col-sm-6">
+                <img src={data.place.pic} alt={data.place.name} />
+                <h3>
+                  Located in {data.place.city}, {data.place.state}
+                </h3>
+              </div>
+              <div className="col-sm-6">
+                <h2>
+                  Rating:
+                </h2>
+                <p>Not Rated</p>
+                <h2>
+                  Description
+                </h2>
+                <h3>
+                  {data.place.showEstablished()}
+                </h3>
+                <h4>
+                  Serving {data.place.cuisines}
+                </h4>
+                <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
+                  Edit
+                </a>
+                <form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
+                  <button type="submit" className="btn btn-danger">
+                    Delete
+                  </button>
+                </form>    
+              </div>
+            </div>
           </main>
-        <h2>
-          Rating:
-        </h2>
-        <p>Not Rated</p>
-        <h2>
-          Description:
-        </h2>
-        <p className="text-center">
-          Located in {data.place.city}, {data.place.state} and Serving           {data.place.cuisines}
-        </p>
-        <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
-          Edit
-        </a>
-        <form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
-          <button type="submit" className="btn btn-danger">
-            Delete
-          </button>
-        </form>    
         </Default>
     )
 }
